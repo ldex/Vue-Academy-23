b6e0f99f008690b4b940c47756a5f7da9fe8c67e
ldex <laurent.duveau+git@gmail.com>
2022-05-02 19:32:39 -0400

    Fin du jour 1

---
+++ src/views/ProductsView.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <section v-if="error">
+            {{error.message}}
+        </section>
+        <section v-else>
+        <div v-if="loading">
+            <h2 class="loading">Loading products...</h2>
+        </div>
+        <product-list v-else :products="products" :page-size="5"></product-list>
+        </section>
+    </div>
+</template>
+
+<script>
+import ProductList from '@/components/ProductList.vue';
+import ProductService from '@/services/ProductService.js';
+
+export default {
+  name: 'app',
+  components: {
+    ProductList
+  },
+  errorCaptured: function(error) {
+      console.error('Error in component: ', error.message);
+      return true;
+  },
+  data() {
+    return {
+      products: [],
+      error: null,
+      loading: false
+    }
+  },
+  created () {
+    this.loading = true;
+    ProductService.getProducts()
+      .then(response => {
+        this.products = response.data;
+      })
+      .catch(error => {
+        this.error = error;
+      })
+      .finally(() => this.loading = false);
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
